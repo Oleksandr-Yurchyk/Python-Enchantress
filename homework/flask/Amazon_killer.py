932848ba0e659bf0f25d40fe8d967ae4dfd1e37d
Oleksandr Yurchyk <yurchyk_sasha@ukr.net>
2021-02-08 21:21:52 +0200

    done two more endpoints

--- homework/flask/Amazon_killer.py
+++ homework/flask/Amazon_killer.py
@@ -8,10 +8,16 @@ USERS_DATABASE = {}
 user_counter = 1
 
 
+class NoSuchUser(Exception):
+    def __init__(self, user_id):
+        self.user_id = user_id
+
+
 @amazon_killer.route('/users', methods=["POST"])
 def create_user():
     global user_counter
     user = request.json
+    user['user_id'] = user_counter
     response = {
         "registration_timestamp": datetime.now().isoformat(),
         "user_id": user_counter
@@ -22,6 +28,21 @@ def create_user():
     user_counter += 1
 
     return response, 201
+
+
+@amazon_killer.errorhandler(NoSuchUser)
+def no_such_user_handler(e):
+    return {"error": "no such user with id 1"}, 404
+
+
+@amazon_killer.route('/users/<int:user_id>')
+def get_user(user_id):
+    try:
+        user = USERS_DATABASE[user_id]
+    except KeyError:
+        raise NoSuchUser(user_id)
+    else:
+        return user
 
 
 if __name__ == '__main__':
